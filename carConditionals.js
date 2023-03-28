22a1dd9eadc66d5176619dc714b589d65f1d1b84
jonathan de alba <JonathanDeAlba@jonathaacintosh.lan>
2023-03-27 23:08:08 -0700

    first commit

---
+++ carConditionals.js
@@ -0,0 +1,52 @@
+  //I'm a user that wants to buy a car that has less then 2000 miles
+  //on it, 
+
+  //write a piece of code that checks if the second object is 
+  //worth buying or not.
+//var cars = [
+  //{
+    //make: "Toyota",
+    //model: "Camry",
+    //year: 2021,
+    //color: "red",
+    //mileage: 2000
+  //},
+  //{
+    //make: "Honda",
+    //model: "Accord",
+    //year: 2022,
+    //color: "blue",
+    //mileage: 1500,
+    //features: ["navigation", "backup camera", "blind spot detection"]
+  //},
+  //{
+    //make: "Tesla",
+    //model: "Model S",
+    //year: 2020,
+    //color: "black",
+    //mileage: 5000,
+    //options: {
+      //autopilot: true,
+      //premiumSound: true,
+      //performance: true
+    //}
+  //},
+  //{
+    //make: "Ford",
+    //model: "F-150",
+    //year: 2023,
+    //color: "white",
+    //mileage: 0,
+    //engine: {
+      //type: "V8",
+      //horsepower: 395,
+      //towingCapacity: "13,200 lbs"
+    //}
+  //}
+//];
+
+if (cars[0].mileage < 2000) {
+  console.log("Yes, we have the car.");
+  } else {
+    console.log("No, we don't have the car.");
+  }
